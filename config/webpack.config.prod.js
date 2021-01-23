4e9d6f7d53fda79bf7d8733fc6a95af990a80df7
ryokoyabu <ryojikoyabu@gmail.com>
2021-01-23 14:07:33 +0800

    trying to fix styles in github pages

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -214,6 +214,15 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          //Added trying to fix styles in github pages
+          {
+            test: /\.scss$/,
+            use: [
+              'style-loader',
+              'css-loader',
+              'sass-loader'
+            ]
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
